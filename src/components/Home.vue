90dca476052f0e177fcdeb1771b31e11b26ffb11
mnapearson <marratoon@gmail.com>
2021-04-27 16:26:02 +0200

    gender filter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,6 +11,9 @@
           <div class="filter-buttons">
             <div class="option date">Date</div>
             <div class="option place">Place</div>
+            <div class="option gender">ALL</div>
+            <div class="detail">saw</div>
+            <div class="option gender">ALL</div>
           </div>
         </div>
         <div class="posts">
