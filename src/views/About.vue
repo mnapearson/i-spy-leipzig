ec55e0aa009d94d146de00aa44c1e605c19f4e3d
mnapearson <marratoon@gmail.com>
2021-05-19 16:31:45 +0200

    add faq details

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <h1>rules</h1>
+    <h1>FAQ</h1>
+    <ol>
+      <li>How do I delete my account?</li>
+      <p>
+        If you wish to delete your account, please contact our team at
+        hi@ispyleipzig.com
+      </p>
+      <li>Can I recover a deleted account?</li>
+      <p>No, we’re unable to recover a deleted account at this time.</p>
+      <li>How do you block or report someone?</li>
+      <p>
+        If you want to delete or block another user, please contact our team at
+        hi@ispyleipzig.com.
+      </p>
+      <li>How do I unlock my account?</li>
+      <p>
+        To unlock your account, please contact our team at hi@ispyleipzig.com.
+      </p>
+      <li>How do I report a fake account?</li>
+      <p>
+        If you believe that another user’s account is fraudulent, please contact
+        our team at hi@ispyleipzig.com.
+      </p>
+      <li>How can I reach out with general questions or concerns?</li>
+      <p>
+        If you have any questions or concerns, please contact our team at
+        hi@ispyleipzig.com.
+      </p>
+    </ol>
+    <h2>
+      Always contact us directly at hi@ispyleipzig.com if you have a question
+      for our team!
+    </h2>
   </div>
 </template>
 
@@ -9,9 +41,26 @@ export default {};
 </script>
 
 <style scoped>
-h1 {
+h1,
+h2 {
   display: flex;
   justify-content: center;
   margin-top: 2rem;
+  width: 70%;
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+ol {
+  margin-top: 3rem;
+  width: 70%;
+}
+
+li {
+  margin-top: 1rem;
+  font-weight: bold;
+}
+p {
+  margin-top: 0.5rem;
 }
 </style>
