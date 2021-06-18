c2094b9e1a3b0d85ebb40663e8be77d42c30325f
jgrdnr <jssgrdnr@gmail.com>
2021-06-18 15:49:31 +0200

    styling changes header

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@
             'background-image': 'url(' + require('./assets/ispyback.png') + ')',
           }"
         >
-          <p class="title">I SPY LEIPZIG</p>
+          <div class="scroll-left">
+            <p class="title">I SPY LEIPZIG</p>
+          </div>
         </div>
       </header>
       <div v-if="!user">
@@ -126,25 +128,32 @@ export default {
 * {
   margin: 0 auto;
   font-family: "Roboto Mono", monospace;
+  cursor: zoom-in;
+}
+.a {
+  cursor: pointer;
+  text-transform: italic;
 }
 
 header {
-  cursor: pointer;
+  cursor: zoom-in;
 }
 
 button {
   text-transform: uppercase;
   margin: 1rem;
+  cursor: pointer;
 }
 
 .title {
   background-color: white;
   display: flex;
   justify-content: center;
-  color: black;
+  color: white;
   mix-blend-mode: screen;
   width: 100vw;
   font-size: 70pt;
+  font-weight: bold;
   text-align: center;
   -webkit-text-stroke: 4px black;
 }
@@ -157,6 +166,7 @@ nav {
 
 .link {
   text-decoration: none;
+  text-transform: italic;
   color: black;
 }
 
@@ -171,11 +181,62 @@ nav {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin-top: 1rem;
   margin-bottom: 3rem;
 }
 
 #mc_embed_signup_scroll {
   display: flex;
   margin-top: 0.5rem;
+}
+.scroll-left {
+  height: 90px;
+  overflow: hidden;
+  position: relative;
+}
+.scroll-left p {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin-top: 10px;
+  line-height: 60px;
+  text-align: center;
+  /* Starting position */
+  -moz-transform: translateX(100%);
+  -webkit-transform: translateX(100%);
+  transform: translateX(100%);
+  /* Apply animation to this element */
+  -moz-animation: scroll-left 10s linear infinite;
+  -webkit-animation: scroll-left 10s linear infinite;
+  animation: scroll-left 30s linear infinite;
+}
+/* Move it (define the animation) */
+@-moz-keyframes scroll-left {
+  0% {
+    -moz-transform: translateX(100%);
+  }
+  100% {
+    -moz-transform: translateX(-100%);
+  }
+}
+@-webkit-keyframes scroll-left {
+  0% {
+    -webkit-transform: translateX(100%);
+  }
+  100% {
+    -webkit-transform: translateX(-100%);
+  }
+}
+@keyframes scroll-left {
+  0% {
+    -moz-transform: translateX(100%); /* Browser bug fix */
+    -webkit-transform: translateX(100%); /* Browser bug fix */
+    transform: translateX(100%);
+  }
+  100% {
+    -moz-transform: translateX(-100%); /* Browser bug fix */
+    -webkit-transform: translateX(-100%); /* Browser bug fix */
+    transform: translateX(-100%);
+  }
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -41,6 +41,9 @@ export default {};
 </script>
 
 <style scoped>
+a {
+  cursor: pointer;
+}
 h1,
 h2 {
   display: flex;
